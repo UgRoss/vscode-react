9cc3c7a126d3bb5e9e37d31578ec6430c60bcf00
Ross <ug.ross@hotmail.com>
2020-05-05 00:05:18 +0300

    Exclude build from git, code improvements

--- src/generateSnippetsPaths.ts
+++ src/generateSnippetsPaths.ts
@@ -1,30 +1,22 @@
-import { writeFile } from 'fs';
-import { spawn } from 'child_process';
-import { getSnippetsFromFolder, writeFileAsync } from './utils';
-
-import {
-  ROOT_DIR,
-  GENERAL_SNIPPETS_DIR,
-  JS_SNIPPETS_DIR,
-  TS_SNIPPETS_DIR,
-  ALL_REACT_LANGUAGES,
-  JS_REACT_LANGUAGES,
-  TS_REACT_LANGUAGES,
-} from './constants';
+import { getSnippetsFromFolder, writeFileAsync, rimrafAsync, mkdirAsync } from './utils';
+import { GENERAL_SNIPPETS_DIR, JS_SNIPPETS_DIR, TS_SNIPPETS_DIR, OUTPUT_DIR } from './constants';
 
 const init = async () => {
-  const generalSnippets = await getSnippetsFromFolder(GENERAL_SNIPPETS_DIR);
-  const jsSnippets = await getSnippetsFromFolder(JS_SNIPPETS_DIR);
-  const tsSnippets = await getSnippetsFromFolder(TS_SNIPPETS_DIR);
+  await rimrafAsync(OUTPUT_DIR);
+  await mkdirAsync(OUTPUT_DIR);
+
+  const generalSnippetsObj = await getSnippetsFromFolder(GENERAL_SNIPPETS_DIR);
+  const jsSnippetsObj = await getSnippetsFromFolder(JS_SNIPPETS_DIR);
+  const tsSnippetsObj = await getSnippetsFromFolder(TS_SNIPPETS_DIR);
 
   writeFileAsync(
-    ROOT_DIR + '/build/javascript.json',
-    JSON.stringify({ ...generalSnippets, ...jsSnippets })
+    OUTPUT_DIR + '/javascript.json',
+    JSON.stringify({ ...generalSnippetsObj, ...jsSnippetsObj })
   );
 
   writeFileAsync(
-    ROOT_DIR + '/build/typescript.json',
-    JSON.stringify({ ...generalSnippets, ...tsSnippets })
+    OUTPUT_DIR + '/typescript.json',
+    JSON.stringify({ ...generalSnippetsObj, ...tsSnippetsObj })
   );
 };
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,34 @@
-import { readFile, readdir, writeFile, write } from 'fs';
+import { readFile, readdir, writeFile, mkdir } from 'fs';
+import rimraf from 'rimraf';
 import { join } from 'path';
 import { promisify } from 'util';
+import { JSON_FILE_REGEX } from './constants';
 
-export const readFileAsync = (fileName: string) => promisify(readFile)(fileName, 'utf8');
-export const readdirAsync = (dirName: string) => promisify(readdir)(dirName, 'utf8');
-export const writeFileAsync = (fileName: string, data: string) =>
-  promisify(writeFile)(fileName, data, { encoding: 'utf8' });
+/** Promisified `fs` methods */
+export const readFileAsync = (filePath: string) => promisify(readFile)(filePath, 'utf8');
+export const readdirAsync = (dirPath: string) => promisify(readdir)(dirPath, 'utf8');
+export const rimrafAsync = (folderPath: string) => promisify(rimraf)(folderPath);
+export const mkdirAsync = (folderPath: string) => promisify(mkdir)(folderPath);
+export const writeFileAsync = (filePath: string, data: string) =>
+  promisify(writeFile)(filePath, data, { encoding: 'utf8' });
 
+/**
+ * Get list if files paths based on directory
+ * @param {string} dirPath - path to directory
+ *
+ * @returns list of files paths inside directory
+ */
 export const getAllFilesInDir = async (dirPath: string): Promise<string[]> => {
   const files = await readdirAsync(dirPath);
   return files.map((f: string) => join(dirPath, f));
 };
 
+/**
+ * Get files content based on paths
+ * @param {string[]} filesPaths - list of paths to files
+ *
+ * @returns list of content of each file
+ */
 export const getFilesContent = async (filesPaths: string[]): Promise<string[]> => {
   return Promise.all(
     filesPaths.map(async filePath => {
@@ -21,11 +38,20 @@ export const getFilesContent = async (filesPaths: string[]): Promise<string[]> =
   );
 };
 
-export const getSnippetsFromFolder = async (folderPath: string) => {
+/**
+ * Get all snippets in object format collected from files in one folder
+ * @param {string} folderPath - folder that contains snippets
+ *
+ *  @returns object with all snippets
+ */
+export const getSnippetsFromFolder = async (folderPath: string): Promise<{}> => {
   const filesPaths = await getAllFilesInDir(folderPath);
-  const filesContent = await getFilesContent(filesPaths);
-  const snippetsObj = filesContent.reduce(
-    (acc, fileContent) => ({ ...acc, ...JSON.parse(fileContent) }),
+  const jsonFiles = filesPaths.filter(filePath => JSON_FILE_REGEX.test(filePath));
+
+  const snippetsContent = await getFilesContent(jsonFiles);
+
+  const snippetsObj = snippetsContent.reduce(
+    (acc, snippetContent) => ({ ...acc, ...JSON.parse(snippetContent) }),
     {}
   );
   return snippetsObj;
